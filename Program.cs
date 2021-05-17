020289e923057d0f04ebf44d4465553c99573149
Felipe <robson.felipe@acad.pucrs.br>
2021-05-17 15:10:17 -0300

    Implements user interaction thought the terminal

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace GettingStarted
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Stranger!");
+
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
